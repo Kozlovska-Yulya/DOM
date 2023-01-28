a0d013c757a02c2e96c08b051ccded105d436e7d
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-01-28 15:03:46 +0100

    find input type 'text'

--- Task-2/index.js
+++ Task-2/index.js
@@ -1,11 +1,11 @@
-export const getTitleElement = () => {
+const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 };
 
-export const getInputElement = () => {
-  const inputElem = document.querySelector('text');
+const getInputElement = () => {
+  const inputElem = document.querySelector('input [type = "text"]');
   console.log(inputElem);
   return inputElem;
 };
