a47524e77961bb8c09d2eacb01e157cd008b0eb6
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-01-29 11:56:27 +0100

    created finish list with numbers in class 'list'

---
+++ Task-9/index.js
@@ -0,0 +1,21 @@
+export const finishList = () => {
+  const listElem = document.querySelector('.list');
+
+  const finishListElem1 = document.createElement('li');
+  finishListElem1.textContent = '8';
+  listElem.append(finishListElem1);
+
+  const finishListElem2 = document.createElement('li');
+  finishListElem2.textContent = '1';
+  listElem.prepend(finishListElem2);
+
+  const listItemElem = document.querySelector('.special');
+  const finishListElem3 = document.createElement('li');
+  finishListElem3.textContent = '4';
+  listItemElem.before(finishListElem3);
+
+  const finishListElem4 = document.createElement('li');
+  finishListElem4.textContent = '6';
+  listItemElem.after(finishListElem4);
+};
+finishList();
