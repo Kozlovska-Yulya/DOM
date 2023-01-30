b4ca91637ceeb99ae3f271c19316a881a1d427a6
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-01-30 15:06:54 +0100

    created input

---
+++ Task-10/index.js
@@ -0,0 +1,12 @@
+const finishForm = () => {
+  const loginElem = document.querySelector('.login-form');
+
+  const inputElem = document.createElement('input');
+  inputElem.setAttribute('type', 'text');
+  inputElem.setAttribute('name', 'login');
+
+  loginElem.append(inputElem);
+
+  const inputElem1 = document.querySelector("input[type='text']");
+  inputElem1.setAttribute('type', 'password');
+};
